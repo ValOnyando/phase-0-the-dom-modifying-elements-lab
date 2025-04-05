dd0926cb7a7d44b463ce30f3eead483b765ea7f0
Vallary Awuor Onyando <valonyando@gmail.com>
2025-04-06 02:01:18 +0300

    Manipulates the DOM

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+let del = document.getElementById("main");
+del.remove();
+
+let newHeader = document.createElement("h1");
+
+newHeader.id = 'victory';
+
+newHeader.innerHTML = '<p>Val is the champion</p>'
